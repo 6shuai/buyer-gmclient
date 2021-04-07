0d19a4a279028c3d0001fcef9b795c48853c69db
liushuai <245691516@qq.com>
2021-04-07 14:06:37 +0800

    创建账号

--- src/api/user.js
+++ src/api/user.js
@@ -14,3 +14,8 @@ export function getAuthMenu(data) { return get('/user/authority', data); }
 //用户列表
 export function getMemberList(data) { return get('/user/', data); }
 
+//添加用户
+export function memberCreate(data) { return post('/user/', data); }
+
+//获取所有角色   (所在部门)
+export function getRoleList(data) { return get('/role/', data); }
--- src/views/member/index.vue
+++ src/views/member/index.vue
@@ -8,7 +8,7 @@
 						type="primary"
 						icon="el-icon-plus"
 						size="small"
-						@click=";(addParams = {}), (addLocationDialog = true)"
+						@click="addParams={enabled: true};addMemberDialog=true"
 					>
 						创建账号
 					</el-button>
@@ -20,7 +20,14 @@
 				</el-table-column>
 				<el-table-column prop="username" label="登录账号" :min-width="120">
 				</el-table-column>
-				<el-table-column prop="city" label="所属部门" :min-width="100">
+				<el-table-column label="所属部门" :min-width="100">
+					<template #default="scope">
+						<el-tag 
+							size="mini"
+							v-for="item in scope.row.roleId" 
+							:key="item"
+						>{{ findRole(item) }}</el-tag>
+					</template>
 				</el-table-column>
 				<el-table-column prop="enabled" label="状态" width="100">
 					<template #default="scope">
@@ -79,50 +86,99 @@
 			width="500px"
 			:close-on-click-modal="false"
 			append-to-body
-			v-model="addLocationDialog"
+			v-model="addMemberDialog"
 		>
-			<el-form label-width="100px">
-				<el-form-item label="姓名">
+			<el-form
+				ref="addMemberForm"
+				:model="addParams"
+				:rules="addRules"
+				label-width="100px"
+			>
+				<el-form-item label="头像" prop="avatar">
+					<upload-img
+						:isArray="false"
+						:imgList="addParams.avatar"
+						@uploadImgPath="uploadImageSuccess"
+					></upload-img>
+				</el-form-item>
+				<el-form-item label="姓名" prop="nickname">
 					<el-input
 						placeholder="请输入用户姓名"
 						v-model="addParams.nickname"
 					></el-input>
 				</el-form-item>
-				<el-form-item label="所在部门">
-					<el-input
-						placeholder="请输入用户所在部门"
-						v-model="addParams.name"
-					></el-input>
+				<el-form-item label="所在部门" prop="roleId">
+					<el-select v-model="addParams.roleId" multiple placeholder="请选择">
+						<el-option
+							v-for="item in roleData"
+							:key="item.id"
+							:label="item.displayName"
+							:value="item.id"
+						>
+						</el-option>
+					</el-select>
 				</el-form-item>
-				<el-form-item label="账号">
+				<el-form-item label="账号" prop="username">
 					<el-input
 						placeholder="请输入登录账号"
-						v-model="addParams.name"
+						v-model="addParams.username"
 					></el-input>
 				</el-form-item>
+				<el-form-item label="密码" prop="password">
+					<el-link
+						type="primary"
+						href="javascript:;"
+						@click="
+							pwParams = {};
+							setPasswordDialog = true
+						"
+						>设置</el-link
+					>
+				</el-form-item>
+			</el-form>
+
+			<template #footer>
+				<span class="dialog-footer">
+					<el-button @click="addMemberDialog = false">取 消</el-button>
+					<el-button
+						type="primary"
+						:loading="btnLoading"
+						@click="handleAddMember"
+						>确 定</el-button
+					>
+				</span>
+			</template>
+		</el-dialog>
+
+		<!-- 设置密码 -->
+		<el-dialog
+			title="设置密码"
+			width="500px"
+			:close-on-click-modal="false"
+			append-to-body
+			v-model="setPasswordDialog"
+		>
+			<el-form label-width="100px">
 				<el-form-item label="密码">
 					<el-input
 						type="password"
 						placeholder="请输入密码"
-						v-model="addParams.password"
+						v-model="pwParams.password"
 					></el-input>
 				</el-form-item>
 				<el-form-item label="确认密码">
 					<el-input
 						type="password"
 						placeholder="请输入确认密码"
-						v-model="addParams.password"
+						v-model="pwParams.confimPassword"
 					></el-input>
 				</el-form-item>
 			</el-form>
 
 			<template #footer>
 				<span class="dialog-footer">
-					<el-button @click="addLocationDialog = false">取 消</el-button>
-					<el-button
-						type="primary"
-						:loading="btnLoading"
-						@click="handleAddLocation"
+					<el-button @click="setPasswordDialog = false">取 消</el-button>
+					<el-button type="primary" :loading="btnLoading" @click="handleSetPw"
 						>确 定</el-button
 					>
 				</span>
@@ -132,19 +188,25 @@
 </template>
 
 <script>
-import { reactive, toRefs, onMounted } from 'vue'
+import { reactive, ref, toRefs, onMounted, getCurrentInstance } from 'vue'
 import { ElMessage } from 'element-plus'
-import { getMemberList } from '@/api/user'
+import { getMemberList, getRoleList, memberCreate } from '@/api/user'
 import Pagination from '@/components/Pagination/index'
+import UploadImg from '@/components/Upload/UploadImg'
 
 export default {
 	components: {
 		Pagination,
+		UploadImg,
 	},
 	setup() {
 		onMounted(() => {
 			init()
+			roleList()
 		})
+
+		const addMemberForm = ref(null)
+		const { proxy } = getCurrentInstance()
 
 		const init = () => {
 			state.loading = true
@@ -153,6 +215,23 @@ export default {
 				let { list, totalRecords } = res.obj
 				state.resData = list
 				state.totalCount = totalRecords
+			})
+		}
+
+		const findRole = id => {
+			let result;
+			state.roleData.forEach(item => {
+				if(item.id == id){
+					result = item.displayName
+				}
+			})
+			return result
+		}
+
+		//角色列表
+		const roleList = () => {
+			getRoleList().then(res => {
+				state.roleData = res.obj
 			})
 		}
 
@@ -168,24 +247,61 @@ export default {
 			init()
 		}
 
+		//头像上传成功
+		const uploadImageSuccess = e => {
+			state.addParams.avatar = e
+		}
+
+		//设置密码
+		const handleSetPw = () => {
+			if (!state.pwParams.password) {
+				ElMessage.warning('请输入密码~')
+				return
+			} else if (!state.pwParams.confimPassword) {
+				ElMessage.warning('请输入确认密码~')
+				return
+			} else if (state.pwParams.password != state.pwParams.confimPassword) {
+				ElMessage.warning('两次密码不一致~')
+				return
+			}
+			state.addParams = {
+				...state.addParams,
+				...state.pwParams,
+			}
+			state.setPasswordDialog = false
+		}
+
 		//添加用户
-		const handleAddLocation = () => {
-			state.btnLoading = true
-			// locationCreate(state.addParams).then(res => {
-			// 	state.btnLoading = false
-			// 	ElMessage.success('添加成功')
-			// })
+		const handleAddMember = () => {
+			state.addMemberForm.validate(valid => {
+				if (valid) {
+					state.btnLoading = true
+					memberCreate(state.addParams).then(res => {
+						state.btnLoading = false
+						if (res.code == proxy.$successCode) {
+							ElMessage.success('添加成功~')
+							state.addMemberDialog = false
+							init()
+						}
+					})
+				}
+			})
 		}
 
 		//编辑用户信息
 		const handleEdit = row => {
 			state.addParams = row
-			state.addLocationDialog = true
+			state.addMemberDialog = true
 		}
 
 		//修改用户状态
 		const handleChangeState = index => {
-			console.log(index)
+			memberCreate(state.resData[index]).then(res => {
+				state.btnLoading = false
+				if (res.code == proxy.$successCode) {
+					ElMessage.success('操作成功~')
+				}
+			})
 		}
 
 		const state = reactive({
@@ -195,15 +311,28 @@ export default {
 				pageNo: 1,
 				pageSize: 40,
 			},
-			addLocationDialog: false,
+			addMemberDialog: false,
+			setPasswordDialog: false,
 			addParams: {}, //添加用户参数
 			resData: [],
 			totalCount: 0,
+			pwParams: {}, //设置密码
+			roleData: [], //所在部门列表
+			addMemberForm,
 			handleSizeChange,
 			handleCurrentChange,
-			handleAddLocation,
+			handleAddMember,
 			handleEdit,
 			handleChangeState,
+			uploadImageSuccess,
+			addRules: {
+				avatar: [{ required: true, message: '请上传头像~', trigger: 'change' }],
+				nickname: [{ required: true, message: '请输入姓名~', trigger: 'blur' }],
+				username: [{ required: true, message: '请输入账号~', trigger: 'blur' }],
+				password: [{ required: true, message: '请输入密码~', trigger: 'blur' }],
+			},
+			handleSetPw,
+			findRole,
 		})
 
 		return toRefs(state)
